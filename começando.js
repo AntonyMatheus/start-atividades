729b54237562271257bd9411c73a9ed69e93c59e
Antony Matheus <105078467+AntonyMatheus@users.noreply.github.com>
2022-06-06 23:54:16 -0300

    Add files via upload

---
+++ começando.js
@@ -0,0 +1,19 @@
+function retornarDados(nome, sobrenome){
+
+    return nome+ " "+sobrenome
+
+}
+
+console.log("antony", "matheus")
+
+
+var subtrair =(c,d){
+
+    return c-d
+
+}
+
+console.log(subtrair(5,3)
+
+
+
