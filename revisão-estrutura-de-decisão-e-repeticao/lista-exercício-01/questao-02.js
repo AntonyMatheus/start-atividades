747bf6af22651a1682c79940ac34587814838d33
Antony Matheus <105078467+AntonyMatheus@users.noreply.github.com>
2022-06-08 23:28:46 -0300

    Delete revisão-estrutura-de-decisão-e-repeticao/lista-exercício-01 directory

--- revisão-estrutura-de-decisão-e-repeticao/lista-exercício-01/questao-02.js
+++
@@ -1,9 +0,0 @@
-// 2)      Faça uma função para calcular o estoque médio de uma peça, sendo que ESTOQUEMÉDIO = (QUANTIDADE MÍNIMA + QUANTIDADE MÁXIMA) /2
-
-function calculoEstoqueMedio(quantidadeMin, quantMax){
-
-let estoqueMedio= (( quantidadeMin + quantMax) /2)
-
-console.log(estoqueMedio)
-
-}
