6c32593bbbc1224982e1a81aa2af89a68269c459
Antony Matheus <105078467+AntonyMatheus@users.noreply.github.com>
2022-06-08 23:31:11 -0300

    Create questao-04.js

---
+++ revisao-estrutura-de-repeticao-e-decisao/lista-exercicio-01/questao-04.js
@@ -0,0 +1,28 @@
+// 4)      Sabendo que A=3, B=7 e C=4, informe se as expressões abaixo são verdadeiras ou falsas.
+
+// a)       (A + C) > B           
+
+// b)      B >= (A + 2)         
+
+// c)       C = (B –A)            
+
+// d)      (B + A) <= C         
+
+// e)      (C + A) > B         
+
+let a = 3
+let b = 7
+let c = 4
+
+console.log((a + b) > c      ) //true
+
+console.log(b >= (a + 2)  ) //true
+
+console.log(c == (b - a)) //true
+
+console.log((b + a) <=b) //false
+
+console.log((c + a) > b) //false
+          
+
+
